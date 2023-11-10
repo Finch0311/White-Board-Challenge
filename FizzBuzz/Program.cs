f25cf6387da03fd66a6ffda8248d33a93a413cbe
Finch0311 <132722383+Finch0311@users.noreply.github.com>
2023-11-10 14:03:02 -0600

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,16 @@
+﻿public string WhiteBoard(int number)
+
+{
+    var userInput = "";    //answer not stored yet
+
+    if(number % 3 == 0)     // if the number is divisible by 3 store "fizz"
+    {
+        userInput = "fizz";
+    }
+    if(number % 5 == 0)     // if the number is divisible by 5 store "fizz" + "buzz" 
+    {
+        userInput += "buzz";
+    }
+    return userInput;     /* since both operations work and store fizz and buzz respectively if answer is 
+                          divisible by 3 and 5 "fizzbuzz" will be potrayed*/   
+}
